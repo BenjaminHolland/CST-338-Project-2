dc44ef7a9b20ebb1f592de171a02fbe6aa0c3925
Benjamin Holland <ben.holland.core@gmail.com>
2017-05-11 14:42:20 -0700

    Added Book Addition UI

---
+++ app/src/main/java/app/csumb2017/cst338/student4338/project2/library/activities/CreateBookActivity.java
@@ -0,0 +1,20 @@
+package app.csumb2017.cst338.student4338.project2.library.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+
+import java.text.DecimalFormat;
+import java.util.regex.Pattern;
+
+import app.csumb2017.cst338.student4338.project2.library.R;
+
+public class CreateBookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_book);
+    }
+}
